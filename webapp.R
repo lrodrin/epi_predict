e6a1a114890e9d58e79ea3579819fb771e891e55
lrodrin <lrodrin@gmail.com>
2023-07-06 11:42:54 +0200

    added shiny webapp call from R script

---
+++ webapp.R
@@ -0,0 +1,10 @@
+install.packages("devtools")
+library(devtools)
+devtools::install_github("lrodrin/SpatialEpiApp") 
+
+
+# Shiny Web Application for the Analysis of Spatial and Spatio-Temporal Disease Data 
+
+
+library(SpatialEpiApp)
+run_app()
