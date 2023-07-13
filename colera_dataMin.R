4e923b2dd165aa2b52a6566332474878312585b5
lrodrin <lrodrin@gmail.com>
2023-07-13 13:09:41 +0200

    added modularization to colera_dataMin.R

--- colera_dataMin.R
+++ colera_dataMin.R
@@ -26,6 +26,151 @@ dir.create(SHAPES_DATA_DIR, showWarnings = FALSE)
 TOTAL_POBLACION_STR <- "Total_poblacion"
 TOTAL_INVASIONES_STR <- paste("Total", INVASIONES_STR, sep = "_")
 TOTAL_DEFUNCIONES_STR <- paste("Total", DEFUNCIONES_STR, sep = "_")
+VALENCIA_STR <- "valencia"
+ARAGON_STR <- "aragon"
+MURCIA_STR <- "murcia"
+MINPOS_SUBSTR <- 18
+MAXPOS_SUBSTR <- 19
+
+
+# functions ---------------------------------------------------------------
+
+
+split_dfByCCAA <- function(df, provincias) {
+  
+  if (is.vector(provincias)) { # more than one county
+    df_ByCCAA <- subset(df, Provincia %in% provincias)
+  }
+  else {
+    df_ByCCAA <- subset(df, Provincia == provincias) # one county
+  }
+  
+  return(df_ByCCAA)
+  
+}
+
+
+agg_invasionesByCCAA <- function(df, nameCCAA) {
+  
+  # aggregate df
+  df_colera_invasiones.agg <- aggregate(
+    x = df$Total_invasiones,
+    by = list(
+      `Codigo Ine` = df$`Codigo Ine`, 
+      Total_poblacion = df$Total_poblacion,
+      Fecha = df$Fecha),
+    FUN = sum
+  )
+  names(df_colera_invasiones.agg) <- c(CODIGO_INE_STR, TOTAL_POBLACION_STR, FECHA_STR, TOTAL_INVASIONES_STR)
+  
+  # order by "Codigo Ine" and "Fecha"  
+  df_colera_invasiones.agg <- df_colera_invasiones.agg[order(df_colera_invasiones.agg$`Codigo Ine`, df_colera_invasiones.agg$Fecha),]
+  # change date format ("1885-06-18") to month (6)
+  df_colera_invasiones.agg$Fecha <- month(as.POSIXlt(df_colera_invasiones.agg$Fecha, format = DATE_FORMAT))
+  # change the index numbers
+  rownames(df_colera_invasiones.agg) <- 1:nrow(df_colera_invasiones.agg)
+  
+  # save aggregated df for Shiny SpatialEpiApp
+  write.csv(
+    df_colera_invasiones.agg,
+    paste0(COLERA_DATA_DIR, "/webapp.colera_total_", INVASIONES_STR, "XCCAA.", nameCCAA, ".csv"),
+    row.names = FALSE
+  )
+  
+  return(df_colera_invasiones.agg)
+  
+}
+
+
+agg_invasionesToMap <- function(df, map) {
+  
+  # reshape aggregated df as wide direction
+  df_colera_invasiones.aggw <-
+    reshape(
+      df,
+      timevar = FECHA_STR,
+      idvar = CODIGO_INE_STR,
+      direction = "wide"
+    )
+  
+  print(df_colera_invasiones.aggw[1:2, ])
+  print(map[1:2, ])
+  
+  # merge map with df_colera_invasiones.aggw
+  map.merged <- merge(map, df_colera_invasiones.aggw, by.x = "CODIGOINE", by.y = CODIGO_INE_STR)
+  print(map.merged[1:2, ])
+  
+  # convert map of type SpatialPolygonsDataFrame to of type sf
+  mapsf <- st_as_sf(map.merged)
+  
+  return(mapsf)
+  
+}
+
+
+plot_invasionesByCCAAxMonth <- function(mapsf, nameCCAA, invasiones_midpoint) {
+  
+  ggplot(mapsf) + geom_sf(aes(fill = Total_invasiones)) +
+    facet_wrap(~Fecha, ncol = 6) +
+    ggtitle(paste0(TOTAL_INVASIONES_STR, " ", nameCCAA, ", ", ANO_STR)) + theme_bw() +
+    theme(
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank()
+    ) +
+    scale_fill_gradient2(
+      midpoint = invasiones_midpoint, low = "blue", mid = "white", high = "red"
+    )
+  
+  # TODO: save plot as paste0(COLERA_PLOTS_DIR, "/colera_total_invasionesXCCAA_", nameCCAA, ".png")
+  
+}
+
+
+gif_invasionesByCCAAxMonth <- function(mapsf, nameCCAA, invasiones_midpoint) {
+  
+  p <- ggplot(mapsf) + 
+    geom_sf(aes(fill = Total_invasiones)) +
+    theme_bw() +
+    theme(
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank()
+    ) +
+    scale_fill_gradient2(
+      midpoint = invasiones_midpoint,
+      low = "blue",
+      mid = "white",
+      high = "red"
+    ) +
+    transition_time(Fecha) +
+    labs(title = "Mes: {round(frame_time, 0)}")
+  
+  animate(p, render = gifski_renderer())
+  anim_save(paste0(COLERA_PLOTS_DIR, "/colera_total_invasionesXCCAA_", nameCCAA, ".gif"))
+  
+}
+
+
+timeplot_invasionesByCCAAxMonth <- function(df) {
+  
+  g <- ggplot(
+    df,
+    aes(
+      x = Fecha,
+      y = Total_invasiones,
+      group = `Codigo Ine`,
+      color = `Codigo Ine`
+    )
+  ) + geom_line() + geom_point(size = 2) + theme_bw()
+  
+  # g
+  # g + theme(legend.position = "none")
+  ggplotly(g)
+  
+  # return(g)
+  
+}
 
 
 # main --------------------------------------------------------------------
@@ -76,13 +221,18 @@ df_colera.merged <-
 
 # unique(df_colera.merged$Provincia)
 
-df_colera.merged.CCAAvalencia <- subset(df_colera.merged, Provincia %in% c("castellon", "valencia"))
-df_colera.merged.CCAAaragon <- subset(df_colera.merged, Provincia %in% c("huesca", "teruel", "zaragoza"))
-df_colera.merged.CCAAmurcia <- subset(df_colera.merged, Provincia == "murcia")
+# split df_colera.merged for CCAA 
+df_colera.split.CCAAvalencia <- split_dfByCCAA(df_colera.merged, c("castellon", VALENCIA_STR))
+df_colera.split.CCAAaragon <- split_dfByCCAA(df_colera.merged, c("huesca", "teruel", "zaragoza"))
+df_colera.split.CCAAmurcia <- split_dfByCCAA(df_colera.merged, MURCIA_STR)
 
-codigosINE.CCAAvalencia <- unique(df_colera.merged.CCAAvalencia$`Codigo Ine`)
-codigosINE.CCAAaragon <- unique(df_colera.merged.CCAAaragon$`Codigo Ine`)
-codigosINE.CCAAmurcia <- unique(df_colera.merged.CCAAmurcia$`Codigo Ine`)
+head(df_colera.split.CCAAvalencia)
+head(df_colera.split.CCAAaragon)
+head(df_colera.split.CCAAmurcia)
+
+codigosINE.CCAAvalencia <- unique(df_colera.split.CCAAvalencia$`Codigo Ine`)
+codigosINE.CCAAaragon <- unique(df_colera.split.CCAAaragon$`Codigo Ine`)
+codigosINE.CCAAmurcia <- unique(df_colera.split.CCAAmurcia$`Codigo Ine`)
 
 
 # map ---------------------------------------------------------------------
@@ -91,6 +241,7 @@ codigosINE.CCAAmurcia <- unique(df_colera.merged.CCAAmurcia$`Codigo Ine`)
 map_municipios <- st_read(paste(SHAPES_DATA_DIR, "MUNICIPIOS_IGN.shp", sep = "/"), quiet = TRUE)
 summary(map_municipios)
 
+# split map_municipios of Spain for CCAA (only "codigosINE" presents in data) 
 map_valencia <- map_municipios[map_municipios$CODIGOINE %in% codigosINE.CCAAvalencia,]
 map_aragon <- map_municipios[map_municipios$CODIGOINE %in% codigosINE.CCAAaragon,]
 map_murcia <- map_municipios[map_municipios$CODIGOINE %in% codigosINE.CCAAmurcia,]
@@ -100,109 +251,65 @@ map_aragon$CODIGOINE <- as.numeric(map_aragon$CODIGOINE)
 map_murcia$CODIGOINE <- as.numeric(map_murcia$CODIGOINE)
 
 plot(map_valencia)
-plot(map_aragon, max.plot = 10)
+plot(map_aragon)
 plot(map_murcia)
 
 
-# invasiones murcia agg ---------------------------------------------------
+# invasiones agg by CCAA --------------------------------------------------
 
 
-head(df_colera.merged.CCAAmurcia)
+df_colera_invasiones.CCAAvalencia.agg <- agg_invasionesByCCAA(df_colera.split.CCAAvalencia, VALENCIA_STR)
+df_colera_invasiones.CCAAaragon.agg <- agg_invasionesByCCAA(df_colera.split.CCAAaragon, ARAGON_STR)
+df_colera_invasiones.CCAAmurcia.agg <- agg_invasionesByCCAA(df_colera.split.CCAAmurcia, MURCIA_STR)
 
-df_colera_invasiones.merged.CCAAmurcia.agg <- aggregate(
-  x = df_colera.merged.CCAAmurcia$Total_invasiones,
-  by = list(
-    `Codigo Ine` = df_colera.merged.CCAAmurcia$`Codigo Ine`, 
-    Total_poblacion = df_colera.merged.CCAAmurcia$Total_poblacion,
-    Fecha = df_colera.merged.CCAAmurcia$Fecha),
-  FUN = sum
-)
+head(df_colera_invasiones.CCAAvalencia.agg)
+head(df_colera_invasiones.CCAAaragon.agg)
+head(df_colera_invasiones.CCAAmurcia.agg)
 
-names(df_colera_invasiones.merged.CCAAmurcia.agg) <- c(CODIGO_INE_STR, TOTAL_POBLACION_STR, FECHA_STR, TOTAL_INVASIONES_STR)
-df_colera_invasiones.merged.CCAAmurcia.agg <- df_colera_invasiones.merged.CCAAmurcia.agg[order(df_colera_invasiones.merged.CCAAmurcia.agg$`Codigo Ine`, df_colera_invasiones.merged.CCAAmurcia.agg$Fecha),]
-df_colera_invasiones.merged.CCAAmurcia.agg$Fecha <- month(as.POSIXlt(df_colera_invasiones.merged.CCAAmurcia.agg$Fecha, format = DATA_FORMAT))
-head(df_colera_invasiones.merged.CCAAmurcia.agg)
 
-# write.csv(df_colera_invasiones.merged.CCAAmurcia.agg, "data.csv", row.names = FALSE)
+# adding invasiones agg to map --------------------------------------------
 
-# adding df_colera_invasiones.merged.CCAAmurcia.agg to map_murcia
-df_colera_invasiones.merged.CCAAmurcia.aggw <-
-  reshape(
-    df_colera_invasiones.merged.CCAAmurcia.agg,
-    timevar = FECHA_STR,
-    idvar = CODIGO_INE_STR,
-    direction = "wide"
-  )
 
-df_colera_invasiones.merged.CCAAmurcia.aggw[1:2, ]
-map_murcia[1:2, ]
+mapsf_valencia <- agg_invasionesToMap(df_colera_invasiones.CCAAvalencia.agg, map_valencia)
+mapsf_aragon <- agg_invasionesToMap(df_colera_invasiones.CCAAaragon.agg, map_aragon)
+mapsf_murcia <- agg_invasionesToMap(df_colera_invasiones.CCAAmurcia.agg, map_murcia)
 
-# merge map_murcia with df_colera_invasiones.merged.CCAAmurcia.aggw
-map_murcia.merged <- merge(map_murcia, df_colera_invasiones.merged.CCAAmurcia.aggw, by.x = "CODIGOINE", by.y = CODIGO_INE_STR)
-map_murcia.merged[1:2, ]
-
-mapsf_murcia.merged <- st_as_sf(map_murcia.merged)
-mapsf_murcia.merged <- gather(mapsf_murcia.merged, Fecha, Total_invasiones, paste0(TOTAL_INVASIONES_STR, ".", 6:11))
-# mapsf_murcia.merged <-
-#   gather(mapsf_murcia.merged,
+mapsf_valencia <- gather(mapsf_valencia, Fecha, Total_invasiones, paste0(TOTAL_INVASIONES_STR, ".", 6:11))
+mapsf_aragon <- gather(mapsf_aragon, Fecha, Total_invasiones, paste0(TOTAL_INVASIONES_STR, ".", 6:11))
+mapsf_murcia <- gather(mapsf_murcia, Fecha, Total_invasiones, paste0(TOTAL_INVASIONES_STR, ".", 6:11))
+# mapsf_murcia <-
+#   gather(mapsf_murcia,
 #          Fecha,
 #          Total_invasiones,
-#          paste0(TOTAL_INVASIONES_STR, ".", seq(
-#            as.Date('1885-06-18'), as.Date('1885-11-18'), by = 1
-#          )))
+#          paste0(TOTAL_INVASIONES_STR, ".", seq(as.Date("1885-06-18"), as.Date("1885-11-18"), by = 1)))
+
+# TODO: try gather by yyyy-mm-dd
 
 
-# invasiones murcia plots -------------------------------------------------
+# invasiones agg plots ----------------------------------------------------
 
 
-# mapsf_murcia.merged$Fecha <- as.Date(substring(mapsf_murcia.merged$Fecha, 18, 30), format = DATA_FORMAT)
-mapsf_murcia.merged$Fecha <- as.integer(substring(mapsf_murcia.merged$Fecha, 18, 19))
-mapsf_murcia.merged[1:2, ]
+# keep integer month in each column like "Total_invasiones_6" in Fecha column
+mapsf_valencia$Fecha <- as.integer(substring(mapsf_valencia$Fecha, MINPOS_SUBSTR, MAXPOS_SUBSTR))
+mapsf_aragon$Fecha <- as.integer(substring(mapsf_aragon$Fecha, MINPOS_SUBSTR, MAXPOS_SUBSTR))
+mapsf_murcia$Fecha <- as.integer(substring(mapsf_murcia$Fecha, MINPOS_SUBSTR, MAXPOS_SUBSTR))
+# mapsf_murcia$Fecha <- as.Date(substring(mapsf_murcia$Fecha, 18, 30), format = DATE_FORMAT)
 
-ggplot(mapsf_murcia.merged) + geom_sf(aes(fill = Total_invasiones)) +
-  facet_wrap(~Fecha, ncol = 6) +
-  ggtitle(paste0(TOTAL_INVASIONES_STR, " murcia, ", ANO_STR)) + theme_bw() +
-  theme(
-    axis.text.x = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks = element_blank()
-  ) +
-  scale_fill_gradient2(
-    midpoint = 100, low = "blue", mid = "white", high = "red"
-  )
+mapsf_valencia[1:2, ]
+mapsf_aragon[1:2, ]
+mapsf_murcia[1:2, ]
 
-g <- ggplot(
-  df_colera_invasiones.merged.CCAAmurcia.agg,
-  aes(
-    x = Fecha,
-    y = Total_invasiones,
-    group = `Codigo Ine`,
-    color = `Codigo Ine`
-  )
-  ) +
-  geom_line() + geom_point(size = 2) + theme_bw()
+# plots by month
+plot_invasionesByCCAAxMonth(mapsf_valencia, VALENCIA_STR, 100)
+plot_invasionesByCCAAxMonth(mapsf_aragon, ARAGON_STR, 180)
+plot_invasionesByCCAAxMonth(mapsf_murcia, MURCIA_STR, 100)
+gif_invasionesByCCAAxMonth(mapsf_valencia, VALENCIA_STR, 100)
+gif_invasionesByCCAAxMonth(mapsf_aragon, ARAGON_STR, 180)
+gif_invasionesByCCAAxMonth(mapsf_murcia, MURCIA_STR, 100)
 
-g
-g + theme(legend.position = "none")
-g + gghighlight(`Codigo Ine` == "30030")
-ggplotly(g)
+# timeplots by month
+timeplot_invasionesByCCAAxMonth(df_colera_invasiones.CCAAvalencia.agg)
+timeplot_invasionesByCCAAxMonth(df_colera_invasiones.CCAAaragon.agg)
+timeplot_invasionesByCCAAxMonth(df_colera_invasiones.CCAAmurcia.agg)
 
-p <- ggplot(mapsf_murcia.merged) + 
-  geom_sf(aes(fill = Total_invasiones)) +
-  theme_bw() +
-  theme(
-    axis.text.x = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks = element_blank()
-  ) +
-  scale_fill_gradient2(
-    midpoint = 100,
-    low = "blue",
-    mid = "white",
-    high = "red"
-  ) +
-  transition_time(Fecha) +
-  labs(title = "Mes: {round(frame_time, 0)}")
-
-animate(p, render = gifski_renderer())
-anim_save("murcia_invasiones.gif")
+# TODO: g + gghighlight(`Codigo Ine` == "30030")
