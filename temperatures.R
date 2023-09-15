69d07b9c7d081181a3a4563457eef63781960534
lrodrin <lrodrin@gmail.com>
2023-09-15 14:15:07 +0200

    added tempe maps

--- temperatures.R
+++ temperatures.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(sf)
 library(dplyr)
 library(tidyr)
+library(scales)
 
 
 # constants ---------------------------------------------------------------
@@ -18,25 +19,14 @@ SHAPES_DATA_DIR <- "shapes"
 dir.create(SHAPES_DATA_DIR, showWarnings = FALSE)
 
 TEMPERATURA_STR <- "temperatura"
+TEMPERATURA_FACTOR_STR <- "temperatura_factor"
 ANO_STR <- "1885"
-NAMEMONTHS_LIST <-
-  c("Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Setiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre"
-  )
-
-NUMMONTHS_LIST <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
 DATE_FORMAT <- "%Y-%m-%d"
 CODIGOINE_STR <- "CODIGOINE"
+NUMMONTHS_LIST <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+NAMEMONTHS_LIST <- c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre")
+PLOT_LABELS <- c("low", "mid-low", "mid-high", "high")
+PLOT_COLORS_BY_LABELS <- c("low" = "#FEE4D8", "mid-low" = "#FCB195", "mid-high" = "#FB795A", "high" = "#BB1419")
 
 
 # functions ---------------------------------------------------------------
@@ -62,13 +52,67 @@ create_tempeTS <- function(df_temperatures, location) {
     ylab("grados (ºC)") + 
     ggtitle(paste0(TEMPERATURA_STR, " mensual ", location, ", ", ANO_STR)) +
     theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
-    scale_x_continuous(
-      breaks = as.numeric(df_temperatures.tmp$mes),
-      labels = format(df_temperatures.tmp$mes, "%b")
-    )
+    scale_x_continuous(breaks = as.numeric(df_temperatures.tmp$mes), labels = format(df_temperatures.tmp$mes, "%b"))
   # )
     
   ggsave(paste0(TEMPE_PLOTS_DIR, "/ts.temperatures_", location, ".png"), dpi = 300, limitsize = TRUE)
+  
+}
+
+
+factorize <- function(df_colera, var_col, factor_col, breaks) {
+  #' Factorize numeric values into discrete intervals.
+  #'
+  #' This function takes a numeric column from a data frame and converts it into factors
+  #' by dividing it into discrete intervals specified by the parameters.
+  #'
+  #' @param df_colera Data frame containing the data.
+  #' @param var_col Name of the column to be factorized.
+  #' @param factor_col Name of the new factor column.
+  #' @param breaks Vector defining the interval breakpoints.
+  #'
+  #' @return The data frame with the new factor column added.
+  
+  df_colera[[factor_col]] <- cut(df_colera[[var_col]], breaks = breaks, labels = PLOT_LABELS)
+  return(df_colera)
+  
+}
+
+
+plot_observationsByMonth <- function(df_colera, var_col, var_colname) {
+  #' Create a list of plots displaying observations by month on spatial maps.
+  #'
+  #' This function generates a list of plots, one for each specified month, displaying observations on spatial maps.
+  #' It uses the provided data frame and variable column to visualize the observations.
+  #'
+  #' @param df_colera Data frame containing the observations.
+  #' @param var_col Column with numeric values for visualization.
+  #' @param var_colname Name of the variable used for colour representation.
+  #'
+  #' @return A list of plots displaying observations by month on spatial maps.
+  
+  plot_list <- list()
+  
+  for (month in as.integer(NUMMONTHS_LIST)) {  
+    
+    month_data <- df_colera[df_colera$mes == month, ]
+    
+    plot <- ggplot() +
+      geom_sf(data = mapS.municipios, aes(), fill = "white", color = "darkgray") +
+      geom_sf(data = month_data, aes(col = !!sym(var_col), fill = !!sym(var_col)), color = "black") +
+      geom_text(data = subset(month_data, NAMEUNIT %in% mapS.municipios$NAMEUNIT), aes(x = longitud, y = latitud, label = NAMEUNIT), size = 2, color = "black", nudge_y = 0.1) + 
+      scale_fill_manual(values = PLOT_COLORS_BY_LABELS) +
+      theme(axis.title = element_blank(), axis.text = element_blank(), axis.ticks = element_blank()) +
+      labs(color = var_colname, fill = var_colname) +
+      theme_void() +
+      ggtitle(paste("month:", month))
+    
+    
+    plot_list[[as.character(month)]] <- plot
+    
+  }
+  
+  return(plot_list)
   
 }
 
@@ -77,12 +121,7 @@ create_tempeTS <- function(df_temperatures, location) {
 
 
 # read temperatures
-df_temperatures <-
-  read_excel(
-    paste(DATA_DIR, "Temperaturas 1885 BBVA Leonardo.xlsx", sep = "/"),
-    sheet = TEMPERATURA_STR,
-    range = cell_rows(4:83)
-  )
+df_temperatures <- read_excel(paste(DATA_DIR, "Temperaturas 1885 BBVA Leonardo.xlsx", sep = "/"), sheet = TEMPERATURA_STR, range = cell_rows(4:83))
 
 # remove NA
 df_temperatures <- na.omit(df_temperatures) 
@@ -106,30 +145,18 @@ df_temperatures.parsed <- df_temperatures.parsed[order(df_temperatures.parsed$lo
 df_temperatures.parsed$mes <- as.character(df_temperatures.parsed$mes)
 
 # rename months from name to number
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[1]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[1], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[2]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[2], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[3]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[3], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[4]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[4], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[5]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[5], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[6]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[6], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[7]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[7], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[8]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[8], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[9]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[9], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[10]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[10], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[11]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[11], sep = "-")
-df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[12]] <-
-  paste(ANO_STR, NUMMONTHS_LIST[12], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[1]] <- paste(ANO_STR, NUMMONTHS_LIST[1], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[2]] <- paste(ANO_STR, NUMMONTHS_LIST[2], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[3]] <- paste(ANO_STR, NUMMONTHS_LIST[3], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[4]] <- paste(ANO_STR, NUMMONTHS_LIST[4], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[5]] <- paste(ANO_STR, NUMMONTHS_LIST[5], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[6]] <- paste(ANO_STR, NUMMONTHS_LIST[6], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[7]] <- paste(ANO_STR, NUMMONTHS_LIST[7], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[8]] <- paste(ANO_STR, NUMMONTHS_LIST[8], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[9]] <- paste(ANO_STR, NUMMONTHS_LIST[9], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[10]] <- paste(ANO_STR, NUMMONTHS_LIST[10], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[11]] <- paste(ANO_STR, NUMMONTHS_LIST[11], sep = "-")
+df_temperatures.parsed$mes[df_temperatures.parsed$mes == NAMEMONTHS_LIST[12]] <- paste(ANO_STR, NUMMONTHS_LIST[12], sep = "-")
 
 # remove NA values
 df_temperatures.parsed <- na.omit(df_temperatures.parsed)
@@ -161,44 +188,54 @@ if(.Platform$OS.type == "windows") {
   Sys.setlocale("LC_TIME", "ca_ES.UTF-8")
 }
 
+# barplot
+ggplot(df_temperatures.parsed, aes(x = localidad, y = temperatura, fill = localidad)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  facet_wrap(~ mes, nrow = 3, scales = "free_x") +  # Utilitzem facet_wrap per controlar les files
+  labs(x = "localidad", y = "grados (ºC)") +
+  scale_y_continuous(breaks = seq(-5, 30, 5), limits = c(-5, 30), labels = number) +
+  ggtitle(paste0("Temperatura mensual por meses en España, ", ANO_STR)) +
+  geom_text(aes(label = temperatura), hjust = -0.2, size = 3, angle = 90) +
+  theme_bw(base_size = 10) +
+  theme(text = element_text(), axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.1), legend.position = "none")
+
+ggsave(paste0(TEMPE_PLOTS_DIR, "/barplot_temperaturesXmunicipios.png"), width = 20, height = 10, dpi = 300, limitsize = TRUE)
+
 
 # map ---------------------------------------------------------------------
 
 
-map_municipios <- st_read(paste(SHAPES_DATA_DIR, "Municipios_IGN.shp", sep = "/"), quiet = TRUE)
-map_municipios <- subset(map_municipios, CODNUT1 != "ES7")
-summary(map_municipios)
+mapS.municipios <- st_read(paste(SHAPES_DATA_DIR, "Municipios_IGN.shp", sep = "/"), quiet = TRUE)
+mapS.municipios <- subset(mapS.municipios, CODNUT1 != "ES7") # remove Canary Islands
+mapS.municipios <- subset(mapS.municipios, !(CODIGOINE %in% c(51001, 52001))) # remove "Ceuta" and "Melilla"
+head(mapS.municipios)
                       
-df_temperatures.parsed$mes <- as.Date(df_temperatures.parsed$mes)
 df_temperatures.parsed$mes <- month(as.POSIXlt(df_temperatures.parsed$mes, format = DATE_FORMAT))
-
-df_temperatures.parsed.w <-
-  reshape(
-    df_temperatures.parsed,
-    timevar = "mes",
-    idvar = "codigo ine",
-    direction = "wide"
+df_temperatures.parsed <- df_temperatures.parsed %>%
+  mutate(
+    latitud = ifelse(latitud < 36.0, 36.0, ifelse(latitud > 43.0, 43.0, latitud)),
+    longitud = ifelse(longitud < -10.0, -10.0, ifelse(longitud > 4.0, 4.0, longitud))
   )
 
-df_temperatures.parsed.w <- na.omit(df_temperatures.parsed.w)
-colnames(df_temperatures.parsed.w)[1] <- CODIGOINE_STR
-df_temperatures.parsed.w$CODIGOINE <- as.character(df_temperatures.parsed.w$CODIGOINE)
+df_temperatures.merged <- merge(mapS.municipios, df_temperatures.parsed, by.x = CODIGOINE_STR, by.y = "codigo ine")
+df_temperatures.merged <- factorize(
+  df_temperatures.merged,
+  TEMPERATURA_STR,
+  TEMPERATURA_FACTOR_STR,
+  c(-4, 6, 12, 18, Inf)
+)
 
-print(df_temperatures.parsed.w[1:2, ])
-print(map_municipios[1:2, ])
+head(df_temperatures.merged)
 
-map_tempe <- left_join(map_municipios, df_temperatures.parsed.w, by = CODIGOINE_STR)
-print(map_tempe[1:2, ])
+plot_list.df_temperatures <- plot_observationsByMonth(df_temperatures.merged, TEMPERATURA_FACTOR_STR, TEMPERATURA_STR)
 
-mapsf_tempe <- st_as_sf(map_tempe)
-mapsf_tempe <- gather(mapsf_tempe, mes, temperatura, paste0(TEMPERATURA_STR, ".", 6:11))
-mapsf_tempe$mes <- as.integer(substring(mapsf_tempe$mes, 13, 14))
+for (month in names(plot_list.df_temperatures)) {
+  # print(plot_list.df_temperatures[[month]])
+  ggsave(paste0(TEMPE_PLOTS_DIR, "/map_temperaturesXmunicipios_", month, ".png"), plot_list.df_temperatures[[month]])
+}
 
-ggplot(mapsf_tempe) + geom_sf(aes(fill = temperatura)) +
-  ggtitle(paste0(TEMPERATURA_STR, " España", ", ", ANO_STR)) + theme_bw() +
-  theme(
-    axis.text.x = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks = element_blank()
-    ) +
-  scale_fill_gradient2(midpoint = 14, low = "blue", mid = "white", high = "red")
+
+# clean environment -------------------------------------------------------
+
+
+rm(df_temperatures, df_temperatures.merged, plot_list.df_temperatures, mapS.municipios, factorize, plot_observationsByMonth)
