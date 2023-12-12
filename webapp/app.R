de17928949a266c97185488b5e49857bba3f08a3
lrodrin <lrodrin@gmail.com>
2023-12-12 20:32:56 +0100

    first skeleton for the webapp

---
+++ webapp/app.R
@@ -0,0 +1,163 @@
+options(shiny.maxRequestSize = 60*1024^2) # set the maximum upload file size to 60 megabytes
+
+
+source("global.R")
+
+
+# UI ----------------------------------------------------------------------
+
+
+ui <- fluidPage(
+  
+  useShinyjs(),
+  
+  # app title
+  tags$head(
+    tags$title("EPI-PREDICT webapp")
+  ),
+  titlePanel(
+    div(
+      h2("EPI-PREDICT webapp", style = "color: #3474A7;"),
+      h4("Shiny webapp for the Exploratory Data Analysis of Spatio-Temporal Cholera Data", style = "color: #555; margin-bottom: 30px;")
+    )
+  ),
+  
+  # sidebar layout with input and output defunitions
+  sidebarLayout(
+    
+    # sidebar panel for inputs
+    sidebarPanel(
+      helpText("File needs to have columns <Codigo Ine><Provincia><Municipio><Fecha><Total_invasiones><Total_defunciones><Total_poblacion>."),
+      fileInput(
+        inputId = "filedata",
+        label = "1. Upload data. Choose csv file",
+        accept = c(".csv")
+      ),
+      helpText("Upload all map files at once: shp, dbf, shx and prj."),
+      fileInput(
+        inputId = "filemap",
+        label = "2. Upload map. Choose shapefile",
+        multiple = TRUE,
+        accept = c(".shp", ".dbf", ".sbn", ".sbx", ".shx", ".prj")
+      ),
+      selectizeInput(
+        inputId = "countyselected",
+        label = "Select county",
+        choices = COUNTIES,
+        selected = COUNTIES[3] # "valencia" county for default
+      ),
+      selectizeInput(
+        inputId = "monthselected",
+        label = "Select month",
+        choices = MONTHS,
+        selected = MONTHS[1] # all months for default
+      ),
+      radioButtons(
+        inputId = "variableselected", 
+        label = "Select variable", 
+        inline = TRUE, 
+        choices = VARIABLES, 
+        selected = VARIABLES[1] # invasions for default
+      ),
+      div(
+        # p("Made with", a("Shiny", href = "http://shiny.rstudio.com")), 
+        img(src = "imageShiny.png", width = "70px", height = "70px"),
+        HTML("<br>")
+      ),
+      div(
+        style = "text-align: center;",
+        HTML("<br>"),
+        p("© 2023 Laura Rodríguez-Navas. All rights reserved.")
+      )
+    ),
+    
+    # main panel for displaying outputs
+    mainPanel(
+      leafletOutput("map"),
+      dygraphOutput("timetrend"),
+      DTOutput("table")
+    )
+  )
+)
+
+
+# server logic ------------------------------------------------------------
+
+
+server <- function(input, output, session) {
+  
+  data <- reactive({
+    req(input$filedata)
+    read.csv(input$filedata$datapath)
+    
+  })
+  
+  map <- reactive({
+    req(input$filemap)
+    shpdf <- input$filemap 
+    
+    tempdirname <- dirname(shpdf$datapath[1])
+    
+    # rename each file in the "datapath" by appending its corresponding "name" in the "shpdf" data frame
+    for (i in 1:nrow(shpdf)) { file.rename(shpdf$datapath[i], paste0(tempdirname, "/", shpdf$name[i])) }
+    
+    # use the "st_read" function from the "rgdal" package to read the shapefile
+    # concatenate the directory name and the file names with the ".shp" extension
+    # filter the file names to include only those ending with ".shp"
+    map <- st_read(paste(tempdirname, shpdf$name[grep(pattern = "*.shp$", shpdf$name)], sep = "/"), quiet = TRUE)
+    map
+    
+  })
+  
+  filtered_data <- reactive({
+    filter_data(data(), input$countyselected, input$monthselected)
+    
+  })
+  
+  output$table <- renderDT({
+    req(filtered_data(), input$countyselected, input$monthselected)
+    rownames(filtered_data) <- NULL
+    datatable(filtered_data())
+    
+  })
+  
+   output$timetrend <- renderDygraph({
+     req(data(), input$countyselected)
+
+     if (input$countyselected != "all") { datafiltered <- subset(data(), Provincia == input$countyselected) }
+     else { datafiltered <- data() }
+
+     dataxts <- NULL
+     municipalities <- unique(datafiltered$`Codigo.Ine`)
+     for (l in 1:length(municipalities)) {
+       datamunicipality <- datafiltered[datafiltered$`Codigo.Ine` == municipalities[l], ]
+       dd <- xts(
+         datamunicipality[, input$variableselected],
+         order.by = as.Date(paste0("1885-", datamunicipality$Fecha, "-01"))
+       )
+       dataxts <- cbind(dataxts, dd)
+     }
+     colnames(dataxts) <- municipalities
+     dygraph(dataxts) %>%
+       dyHighlight(highlightSeriesBackgroundAlpha = 0.2) -> d1
+     d1$x$css <- "
+  .dygraph-legend > span {display:none;}
+  .dygraph-legend > span.highlight { display: inline; }
+  "
+     d1
+   })
+  
+  output$map <- renderLeaflet({
+    req(filtered_data(), map(), input$countyselected, input$monthselected)
+    
+    # add data to map
+    mapfiltered <-  merge(map(), filtered_data(), by.x = CODIGOINE_STR, by.y = CODIGO_INE_STR)
+    
+    # create tmap
+    l <- tmap_leaflet(create_tmap(mapfiltered, mapfiltered, input$variableselected, "jenks"))
+    
+  })
+}
+
+# run the app
+shinyApp(ui = ui, server = server)
---
+++ webapp/global.R
@@ -0,0 +1,60 @@
+library(shiny)
+library(shinyjs)
+library(sf)
+library(rgdal)
+library(DT)
+library(dygraphs)
+library(xts)
+library(leaflet)
+library(tmap)
+
+
+# constants ---------------------------------------------------------------
+
+
+CODIGOINE_STR <- "CODIGOINE"
+CODIGO_INE_STR <- "Codigo.Ine"
+COUNTIES <- c("all", "zaragoza", "valencia", "granada", "murcia", "teruel", "castellon", "alicante", "navarra", "cuenca", "albacete")
+MONTHS <- c("all", "6", "7", "8", "9", "10", "11")
+VARIABLES <- c("Total_invasiones", "Total_defunciones", "Total_poblacion")
+
+
+# functions ---------------------------------------------------------------
+
+
+create_tmap <- function(df_mes, map, var_col, style) {
+  
+  map.tmp <- tm_shape(df_mes, bbox = map) +
+    tm_polygons(
+      col = var_col,
+      border.col = NULL,
+      title = var_col,
+      palette = "Reds",
+      style = style
+    ) +
+    tm_shape(map) + tm_borders() +
+    tm_layout(
+      legend.text.size = 1.5,
+      inner.margins = c(0, 0, 0, 0),
+      panel.label.size = 1.5, panel.label.height = 1.1, 
+      panel.label.color = "black", panel.label.bg.color = "gray"
+    )
+  
+  return(map.tmp)
+}
+
+
+filter_data <- function(data, county_selected, month_selected) {
+  
+  filtered_data <- data
+  
+  if (county_selected != "all") { filtered_data <- subset(filtered_data, Provincia == county_selected) }
+  if (month_selected != "all") { filtered_data <- subset(filtered_data, Fecha == month_selected) } 
+  else {
+    filtered_data <- filtered_data %>% group_by(`Codigo.Ine`, Provincia, Municipio, Total_poblacion) %>% 
+      summarize(Total_invasiones = sum(Total_invasiones), Total_defunciones = sum(Total_defunciones))
+  }
+  
+  return(filtered_data)
+}
+
