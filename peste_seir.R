42552e46ea1cf402c6e0672a0a591f3a1ac7273d
lrodrin <lrodrin@gmail.com>
2024-02-22 18:35:40 +0100

    updated peste_seir.R by city

--- peste_seir.R
+++ peste_seir.R
@@ -2,33 +2,38 @@ library(dplyr)
 library(lubridate)
 library (deSolve) 
 
-category_mapping <-
+
+load("peste_data.RData")
+
+
+# constants ---------------------------------------------------------------
+
+
+SEIR_COMPARTMENTS <-  c("Susceptible", "Exposed", "Infectious", "Recovered")
+CATEGORY_MAPPING <-
   c(
-    "Enfermos existentes segun parte anterior" = "Infectious",
-    "Nuevamente acometidos" = "Infectious",
-    "Muertos" = "Recovered",
-    "Pasados a convalecientes" = "Recovered",
-    "Curados" = "Recovered",
-    "Total de enfermos que restan" = "Exposed"
+    "Enfermos existentes segun parte anterior" = SEIR_COMPARTMENTS[3], # infectious
+    "Nuevamente acometidos" = SEIR_COMPARTMENTS[3], # infectious
+    "Pasados a convalecientes" = SEIR_COMPARTMENTS[4], # recovered
+    "Curados" = SEIR_COMPARTMENTS[4], # recovered
+    "Total de enfermos que restan" = SEIR_COMPARTMENTS[2] # exposed
   )
 
-# Convert "Categoria" column to the corresponding SEIR compartment
-df_peste$Categoria <- category_mapping[df_peste$Categoria]
 
-# Choose "Artà"
-df_peste.arta <- subset(df_peste, Municipio == "Artà")
+# functions ---------------------------------------------------------------
 
-# Calculate "Total_Casos"
-df_peste.arta <- df_peste.arta %>%
-  # Group by "Municipio", "Categoria" and "Fecha"
-  group_by(Municipio, Categoria, Fecha) %>%
-  # Sum the cases for each group
-  summarise(Total_Casos = sum(Casos)) %>%
-  # Ungroup the data
-  ungroup()
 
-# Function to compute derivatives of the differential equations
-seir_model <- function (current_timepoint, state_values, parameters) {
+seir_model <- function(current_timepoint, state_values, parameters) {
+  #' SEIR Model Function
+  #'
+  #' This function implements the SEIR (Susceptible-Exposed-Infectious-Recovered) model 
+  #' to compute the derivatives of each compartment with respect to time.
+  #'
+  #' @param current_timepoint The current time point.
+  #' @param state_values A vector containing the current values of the state variables (S, E, I, R).
+  #' @param parameters A list containing the parameters of the SEIR model (beta, delta, gamma).
+  #' 
+  #' @return A list containing the derivatives of each compartment (dS, dE, dI, dR).
   
   # create state variables (local variables)
   S = state_values[1]        # susceptible
@@ -52,53 +57,83 @@ seir_model <- function (current_timepoint, state_values, parameters) {
   )
 }
 
-# Parameters
-contact_rate = 10                     # number of contacts per day
-transmission_probability = 0.07       # transmission probability
-infectious_period = 5                 # infectious period
-latent_period = 2                     # latent period
 
-# Compute values of beta (tranmission rate) and gamma (recovery rate)
-beta_value = contact_rate * transmission_probability
-gamma_value = 1 / infectious_period
-delta_value = 1 / latent_period
+run_seir <- function(df, localidad, npopulation, x) {
+  #' Run SEIR Epidemic Simulation
+  #'
+  #' This function simulates the SEIR (Susceptible-Exposed-Infectious-Recovered) epidemic model 
+  #' for a given sub-population defined by a specific locality.
+  #'
+  #' @param df A data frame containing the epidemic data with columns for "Municipio" (locality), 
+  #' "Categoria" (epidemic category), and "Fecha" (date).
+  #' @param localidad The name of the locality (sub-population) for which the simulation will be run.
+  #' @param npopulation The total population of the locality.
+  #' 
+  #' @return NULL. The function generates a plot showing the dynamics of the Susceptible, Exposed, 
+  #' Infectious, and Recovered sub-populations over time.
+  
+  # choose sub-population by localidad
+  df_tmp <- subset(df, Municipio == localidad)
+  
+  # calculate "Total_Casos"
+  df_tmp <- df_tmp %>%
+    # Group by "Municipio", "Categoria" and "Fecha"
+    group_by(Municipio, Categoria, Fecha) %>%
+    # sum the cases for each group
+    summarise(Total_Casos = sum(Casos)) %>%
+    # ungroup the df_tmp
+    ungroup()
+  
+  # parameters
+  contact_rate <- as.integer(mean(df_tmp$Total_Casos))     # number of contacts per day
+  transmission_probability <- 0.07                         # transmission probability
+  infectious_period <- 5                                   # infectious period
+  latent_period <- 2                                       # latent period
+  
+  # compute values of beta (transmission rate) and gamma (recovery rate)
+  beta_value <- contact_rate * transmission_probability
+  gamma_value <- 1 / infectious_period
+  delta_value <- 1 / latent_period
+  
+  # compute Ro - reproductive number
+  Ro <- beta_value / gamma_value
+  
+  # disease dynamics parameters
+  parameter_list <- c(beta = beta_value, gamma = gamma_value, delta = delta_value)
+  
+  # initial values for sub-population
+  X <- nrow(df_tmp[df_tmp$Categoria == SEIR_COMPARTMENTS[3], ]) # infectious
+  Y <- nrow(df_tmp[df_tmp$Categoria == SEIR_COMPARTMENTS[4], ]) # recovered
+  Z <- nrow(df_tmp[df_tmp$Categoria == SEIR_COMPARTMENTS[2], ]) # exposed
+  W <- npopulation - X - Y - Z # Susceptible = Total population - Infectious - Recovered - Exposed 
+  N <- npopulation # Total population
+  
+  # initial state values for the differential equations
+  initial_values <- c(S = W/N, E = X/N, I = Y/N, R = Z/N)
+  timepoints <- seq(0, nrow(df_tmp) - 1, by = 1)
+  print(unique(df_tmp$Fecha))
+  
+  # simulate the SEIR epidemic
+  model_seir <- lsoda(initial_values, timepoints, seir_model, parameter_list)
+  
+  # plot dynamics of Susceptible, Exposed, Infectious and Recovered sub-population 
+  plot(S ~ time, data = model_seir, type = "b", ylim = c(0, 1), col = "blue", xlab = "Time (days)", ylab = "Proportion of individuals", main = paste0("SEIR Epidemic - ", localidad))
+  lines(E ~ time, data = model_seir, type = "b", col = "pink")
+  lines(I ~ time, data = model_seir, type = "b", col = "red")
+  lines(R ~ time, data = model_seir, type = "b", col = "green")
+  abline(v = x, col = "black", lty = "dashed") 
+  legend("right", legend = SEIR_COMPARTMENTS, col = c("blue", "pink", "red", "green"), lty = 1, bty = "n", inset = c(0.01, 0.05))
+}
 
-# Compute Ro - Reproductive number
-Ro = beta_value / gamma_value
 
-# Disease dynamics parameters
-parameter_list = c (beta = beta_value, gamma = gamma_value, delta = delta_value)
+# main --------------------------------------------------------------------
 
-# Initial values for sub-population
-X <- nrow(df_peste.arta[df_peste.arta$Categoria == "Infectious", ])
-Y <- nrow(df_peste.arta[df_peste.arta$Categoria == "Recovered", ])
-Z <- nrow(df_peste.arta[df_peste.arta$Categoria == "Exposed", ])
-W <- 3626 - X - Y - Z # Susceptible = Total population - Infectious - Recovered - Exposed 
-N <- 3626 # Total population
 
-# Initial state values for the differential equations
-initial_values <- c (S = W/N, E = X/N, I = Y/N, R = Z/N)
-timepoints <- seq(0, nrow(df_peste.arta) - 1, by = 1)
+# convert "Categoria" to the corresponding SEIR compartment
+df_peste$Categoria <- CATEGORY_MAPPING[df_peste$Categoria]
 
-# Simulate the SEIR epidemic
-output <- lsoda (initial_values, timepoints, seir_model, parameter_list)
-
-# Plot dynamics of Susceptibles, Exposed, Infectious and Recovered sub-populations in the same plot
-plot(S ~ time, data = output, type = 'b', ylim = c(0, 1), col = 'blue', ylab = 'Population', main = 'SEIR Epidemic - Artà')
-lines(E ~ time, data = output, type = 'b', col = 'pink')
-lines(I ~ time, data = output, type = 'b', col = 'red')
-lines(R ~ time, data = output, type = 'b', col = 'green')
-legend('topright', legend = c("Susceptible", "Exposed", "Infectious", "Recovered"), col = c('blue', 'pink', 'red', 'green'), lty = 1, bty = 'n', inset = c(0.02, 0.02))
-
-output_df <- as.data.frame(output)
-
-# Crear el gráfico SEIR con ggplot2
-ggplot(output_df, aes(x = time)) +
-  geom_line(aes(y = S, color = "Susceptible")) +
-  geom_line(aes(y = E, color = "Exposed")) +
-  geom_line(aes(y = I, color = "Infectious")) +
-  geom_line(aes(y = R, color = "Recovered")) +
-  scale_color_manual(values = c("Susceptible" = "blue", "Exposed" = "pink", "Infectious" = "red", "Recovered" = "green")) +
-  labs(title = "SEIR Epidemic - Artà", y = "Population") +
-  theme_minimal() +
-  theme(legend.position = "top")
+# simulate the SEIR epidemic by "Municipio"
+run_seir(df_peste, LOCALIDADES_STR[1], 3626, 10) # Artà, 204 days
+run_seir(df_peste, LOCALIDADES_STR[2], 1179, 8) # Capdepera, 206 days
+# run_seir(df_peste, LOCALIDADES_STR[3], 1338) # Sant Llorenç des Cardassar, 78 days
+run_seir(df_peste, LOCALIDADES_STR[4], 1684, 10) # Son Servera, 210 days
