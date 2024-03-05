b8f3f3385537afa3066c1be9a32467dbd790a7dd
lrodrin <lrodrin@gmail.com>
2024-03-05 17:37:44 +0100

    adapted colera_stp.R for cases and deaths

--- colera_stp.R
+++ colera_stp.R
@@ -4,7 +4,7 @@ library(sf)
 library(spdep)
 library(ggplot2)
 library(tmap)
-
+library(sp)
 
 load("colera_data.RData") # load cholera data
 
@@ -19,6 +19,7 @@ dir.create(SHAPES_DATA_DIR, showWarnings = FALSE)
 
 PROVINCIAS_STR <- c("zaragoza", "valencia", "granada", "murcia", "teruel", "castellon", "alicante", "navarra", "cuenca", "albacete")
 TOTAL_INVASIONES_STR <- paste("Total", INVASIONES_STR, sep = "_")
+TOTAL_DEFUNCIONES_STR <- paste("Total", DEFUNCIONES_STR, sep = "_")
 COLORS <- c("#ffffff", "#2c7bb6", "#abd9e9", "#fdae61", "#d7191c")
 COLORS_SPOTS <- c("#d1e5f0", "#f7f7f7", "#fddbc7", "#f4a582", "#d6604d", "#b2182b")
 CLUSTERS <- c("insignificant", "low-low", "low-high", "high-low", "high-high")
@@ -61,46 +62,48 @@ compute_weight_matrices <- function(data, province) {
 }
 
 
-compute_moran <- function(data, province) {
+compute_moran <- function(data, province, cause) {
   #' Compute Moran's I.
   #'
   #' This function computes Moran's I for a given spatial data frame.
   #'
   #' @param data A spatial data frame.
   #' @param province Name of the province.
+  #' @param cause Cause of the disease. Invasion or death.
 
   for(i in sort(unique(data$Fecha))) { # iterate over 6 to 11 (june to november)
 
     var_names <- paste0("knb_lw.", province, i)
     knb <- get(var_names, envir = globalenv()) # get listw object
 
-    localMI  <- paste0("lmoran_month.", province, i) # create local moran's I
-    cm_localMI <- paste0("cm_localMI_month.", province, i) # create combined local moran's I
+    localMI  <- paste0("lmoran_month.", province, i, ".", cause) # create local moran's I
+    cm_localMI <- paste0("cm_localMI_month.", province, i, ".", cause) # create combined local moran's I
 
-    assign(localMI, localmoran(data[data$Fecha == i, ][[TOTAL_INVASIONES_STR]], knb), envir = .GlobalEnv) # compute local moran's I
+    assign(localMI, localmoran(data[data$Fecha == i, ][[cause]], knb), envir = .GlobalEnv) # compute local moran's I
     assign(cm_localMI, cbind(data[data$Fecha == i, ], eval(as.name(localMI))), envir = .GlobalEnv) # combine local moran's I with data
   }
 }
 
 
-compute_lisaClusters <- function(data, province) {
+compute_lisaClusters <- function(data, province, cause) {
   #' Compute LISA clusters.
   #'
   #' This function computes LISA clusters for a given spatial data frame.
   #'
   #' @param data A spatial data frame.
   #' @param province Name of the province.
+  #' @param cause Cause of the disease. Invasion or death.
 
   LisaList <- list() # create list to store LISA clusters
   LisaPlot <- list() # create list to store LISA plots
 
   for(i in sort(unique(data$Fecha))) { # iterate over 6 to 11 (june to november)
 
-    localMI <- paste0("lmoran_month.", province, i) # get local moran's I
+    localMI <- paste0("lmoran_month.", province, i, ".", cause) # get local moran's I
     quadrant <- vector(mode = "numeric", length = nrow(eval(as.name(localMI)))) # create vector to store quadrants
 
     # compute deviations
-    DV <- data[data$Fecha == i, ][[TOTAL_INVASIONES_STR]] - mean(data[data$Fecha == i, ][[TOTAL_INVASIONES_STR]])
+    DV <- data[data$Fecha == i, ][[cause]] - mean(data[data$Fecha == i, ][[cause]])
     C_mI <- eval(as.name(localMI))[,1]- mean(eval(as.name(localMI))[,1])
     signif <- 0.05
     quadrant[DV >0 & C_mI>0] <- 4
@@ -109,7 +112,7 @@ compute_lisaClusters <- function(data, province) {
     quadrant[DV >0 & C_mI<0] <- 3
     quadrant[eval(as.name(localMI))[,5]>signif] <- 0
     
-    cm_localMI <- paste0("cm_localMI_month.", province, i) # create combined local moran's I
+    cm_localMI <- paste0("cm_localMI_month.", province, i, ".", cause) # create combined local moran's I
     
     quadrant_df <- data.frame("quadrant" = quadrant) # create data frame with quadrants
     assign(cm_localMI, cbind(eval(as.name(cm_localMI)), quadrant_df), envir = .GlobalEnv) # combine combined local moran's I with quadrants
@@ -123,17 +126,18 @@ compute_lisaClusters <- function(data, province) {
       tm_view(set.zoom.limits = c(11, 17))+
       tm_layout(paste("month", i), legend.outside = TRUE)
   }
-  tmap_save(tmap_arrange(LisaPlot[6:11], ncol = 2), paste(COLERA_MAPS_DIR, paste0("lisaClusters_map.", province, ".png"), sep = "/")) # save LISA clusters plot
+  tmap_save(tmap_arrange(LisaPlot[6:11], ncol = 2), paste(COLERA_MAPS_DIR, paste0("lisaClusters_map.", province, ".", cause,".png"), sep = "/")) # save LISA clusters plot
 }
 
 
-compute_localGi <- function(data, province) {
+compute_localGi <- function(data, province, cause) {
     #' Compute local G_i.
     #'
     #' This function computes local G_i for a given spatial data frame.
     #'
     #' @param data A spatial data frame.
     #' @param province Name of the province.
+    #' @param cause Cause of the disease. Invasion or death.
 
     localGiList <- list() # create list to store local G_i
     localGiPlot <- list() # create list to store local G_i plots
@@ -142,11 +146,11 @@ compute_localGi <- function(data, province) {
 
       var_names <- paste0("knb_lw.", province, i)
       knb <- get(var_names, envir = globalenv()) # get listw object
-      localGI <- paste0("gi_month", i) # create local G_i
-      assign(localGI, cbind(data[data$Fecha == i, ], data.frame("gstat_adaptive" = as.matrix(localG(data[data$Fecha == i, ][[TOTAL_INVASIONES_STR]], knb)))), envir = .GlobalEnv) # compute local G_i
+      localGI <- paste0("gi_month", i, ".", cause) # create local G_i
+      assign(localGI, cbind(data[data$Fecha == i, ], data.frame("gstat_adaptive" = as.matrix(localG(data[data$Fecha == i, ][[cause]], knb)))), envir = .GlobalEnv) # compute local G_i
 
       categorized_data <- transform(get(localGI), category = cut(gstat_adaptive, breaks = c(-Inf, 0, 1, 2, 3, 4, 5), labels = CLUSTERS_SPOTS,include.lowest = TRUE)) # categorize data
-      assign(localGI, cbind(data[data$Fecha == i, ], data.frame("gstat_adaptive" = as.matrix(localG(data[data$Fecha == i, ][[TOTAL_INVASIONES_STR]], knb))), categorized_data), envir = .GlobalEnv) # combine local G_i with categorized data
+      assign(localGI, cbind(data[data$Fecha == i, ], data.frame("gstat_adaptive" = as.matrix(localG(data[data$Fecha == i, ][[cause]], knb))), categorized_data), envir = .GlobalEnv) # combine local G_i with categorized data
 
       localGiList[[i]] <- get(localGI) # store local G_i
 
@@ -157,7 +161,7 @@ compute_localGi <- function(data, province) {
         tm_view(set.zoom.limits = c(11, 17))+
         tm_layout(paste("month", i), legend.outside = TRUE)
     }
-    tmap_save(tmap_arrange(localGiPlot[6:11], ncol = 2), paste(COLERA_MAPS_DIR, paste0("hotspots_map.", province, ".png"), sep = "/")) # save local G_i plot
+    tmap_save(tmap_arrange(localGiPlot[6:11], ncol = 2), paste(COLERA_MAPS_DIR, paste0("hotspots_map.", province, ".", cause, ".png"), sep = "/")) # save local G_i plot
 }
 
 
@@ -187,9 +191,13 @@ for(provincia in PROVINCIAS_STR) {
   spdf_mapS <- get(var_names, envir = globalenv())
 
   compute_weight_matrices(spdf_mapS, provincia) # compute weight matrices
-  compute_moran(spdf_mapS, provincia) # compute moran's I
-  compute_lisaClusters(spdf_mapS, provincia) # compute LISA clusters
-  compute_localGi(spdf_mapS, provincia) # compute local G_i
+
+  # compute moran's I, LISA clusters and local G_i for total invasions and total deaths
+  for(cause in c(TOTAL_INVASIONES_STR, TOTAL_DEFUNCIONES_STR)) {
+    compute_moran(spdf_mapS, provincia, cause) # compute moran's I
+    compute_lisaClusters(spdf_mapS, provincia, cause) # compute LISA clusters
+    compute_localGi(spdf_mapS, provincia, cause) # compute local G_i
+  }
 }
 
 rm(list = ls())
